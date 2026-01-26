ef5226447732f1b387b3dd1d434839b76a8a1517
Rad Albarati <albaratr3197@student.faytechcc.edu>
2026-01-26 00:24:08 +0000

    finished M1LAB1

--- M1LAB1_Radical-coding.cpp
+++ M1LAB1_Radical-coding.cpp
@@ -10,6 +10,27 @@ Radical-coding
 using namespace std;
 
 int main() {
+    // This program will simulate an apple orchard.
+    // The owner’s name
+    string name = "Jane Smith";
+    // number of apples owned
+    int apples = 100;
+    // price per apple
+    double pricePerApple = 0.25;
 
-    
+    // calculate the total price of the apples
+    double totalPrice = apples * pricePerApple;
+
+    // print all the information about the orchard
+    cout << "Welcome to " << name;
+    cout << " apple orchard." << endl;
+    cout << "We have " << apples;
+    cout << " apples in stock" << endl;
+    cout << "Apples are currently $";
+    cout << pricePerApple << " each." << endl;
+
+    // Final line, to print totalPrice
+    cout << "If you want them all, that will be $" <<
+    totalPrice << endl;
+
 }
