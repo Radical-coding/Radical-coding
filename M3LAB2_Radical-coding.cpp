f1da1bfb2dd08c3ea377919ef9868620929518f6
Rad Albarati <albaratr3197@student.faytechcc.edu>
2026-03-17 16:37:31 +0000

    finished

--- M3LAB2_Radical-coding.cpp
+++ M3LAB2_Radical-coding.cpp
@@ -24,4 +24,18 @@ int main() {
     } 
     else if (grade >= 70 && grade <= 79) {
         cout << "Your letter grade is: C" << endl;
-   
+    } 
+    else if (grade >= 60 && grade <= 69) {
+        cout << "Your letter grade is: D" << endl;
+    } 
+    else if (grade >= 0 && grade <= 59) {
+        cout << "Your letter grade is: F" << endl;
+    } 
+    else {
+        // Handle edge cases outside the standard 0-100 range
+        cout << "Invalid grade entered. Please restart and enter a number between 0 and 100." << endl;
+    }
+
+    // End the program
+    return 0;
+}
